638a7455e36c74f279690c8f7355c92805b331cb
CodeOS99 <kush.kuhu@gmail.com>
2026-03-08 14:08:26 +0530

    Changed the axe chopping animation, and changed it from constantly chopping to chopping everytime lmb is pressed

--- scripts/axe_tool.gd
+++ scripts/axe_tool.gd
@@ -1,7 +1,14 @@
 extends Node3D
 
+var using: bool = false
+
 func use():
 	$AnimationPlayer.play("lmb")
+	using = true
 
 func reset():
-	$AnimationPlayer.play("RESET")
+	pass # nothing needs to happen if the player stop pressing lmb
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "lmb":
+		using = false
