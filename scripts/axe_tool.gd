1b12f991c4ee66e5ed68b21f95e49a2e9ecf1caa
CodeOS99 <kush.kuhu@gmail.com>
2026-03-08 14:22:33 +0530

    The axe now detects trees and chops them, only one time in one swing. Current only the tree's scale is reduced

--- scripts/axe_tool.gd
+++ scripts/axe_tool.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
+@onready var area: Area3D = $Area3D
+
 var using: bool = false
+var hit_trees = [] # if performance becomes an issue i could give every tree and index, and then binary search along this array to find if the tree exists although i don't think it will be an issue
 
 func use():
 	$AnimationPlayer.play("lmb")
@@ -12,3 +15,15 @@ func reset():
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "lmb":
 		using = false
+		hit_trees = []
+
+func _process(delta: float) -> void:
+	for body in area.get_overlapping_bodies():
+		if body.is_in_group("Tree") and body not in hit_trees:
+			body.mesh.scale.x-=1
+			body.mesh.scale.y-=1
+			body.mesh.scale.z-=1
+			if body.scale.x == 0:
+				body.queue_free()
+			hit_trees.append(body)
+			
